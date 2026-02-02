34f1ea5a08e64d22b71e0a63096e755e4f4ae18d
Lopezzz56 <frank.lopes@xaviers@edu.in>
2026-02-02 14:04:09 +0530

    Stream Addition

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser, upsertStreamUser } from "./steam.js";
 
 export const inngest = new Inngest({ id: "talent-iq" });
 
@@ -20,6 +21,11 @@ const syncUser = inngest.createFunction(
     };
 
     await User.create(newUser);
+    await upsertStreamUser({
+        id: newUser.clerkId.toString(),
+        name: newUser.name,
+        image: newUser.profileImage
+    })
   }
 );
 
@@ -32,6 +38,8 @@ const deleteUserFromDB = inngest.createFunction(
     const { id } = event.data;
 
     await User.deleteOne({ clerkId: id });
+
+    await deleteStreamUser(id.toString());
   }
 );
 
---
+++ backend/src/lib/steam.js
@@ -0,0 +1,32 @@
+import { StreamChat} from "stream-chat"
+import { ENV } from "./env"
+
+const apiKey = ENV.STREAM_API_KEY
+const apiSecret = ENV.STREAM_API_SECRET
+
+if(!apiKey || !apiSecret){
+    console.error("Stream api key or Stream api secret is missing")
+}
+
+
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const upsertStreamUser = async(userData) => {
+    try{
+await chatClient.upsertUser(userData)
+console.log("Stream user Upserted Successfully", userData)
+    }catch(error){
+console.error("Error Upserting Stream user", error)
+    }
+
+}
+
+export const deleteStreamUser = async(userId) => {
+    try{
+await chatClient.deleteUser({userId})
+console.log("Stream user Deleted ", userId)
+    }catch(error){
+console.error("Error Deleting Stream user", error)
+    }
+    
+}
