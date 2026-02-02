d26b054747dab6198b5d20a5448241fe27af772b
Lopezzz56 <frank.lopes@xaviers@edu.in>
2026-02-02 14:18:39 +0530

    Stream Addition

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,5 +1,5 @@
 import { StreamChat} from "stream-chat"
-import { ENV } from "./env"
+import { ENV } from "./env.js"
 
 const apiKey = ENV.STREAM_API_KEY
 const apiSecret = ENV.STREAM_API_SECRET
@@ -13,6 +13,8 @@ export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
 
 export const upsertStreamUser = async(userData) => {
     try{
+console.log("Upserting to Stream...");
+
 await chatClient.upsertUser(userData)
 console.log("Stream user Upserted Successfully", userData)
     }catch(error){
@@ -23,7 +25,7 @@ console.error("Error Upserting Stream user", error)
 
 export const deleteStreamUser = async(userId) => {
     try{
-await chatClient.deleteUser({userId})
+await chatClient.deleteUser(userId)
 console.log("Stream user Deleted ", userId)
     }catch(error){
 console.error("Error Deleting Stream user", error)
